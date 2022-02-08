cc07e7d7ffd206e4596a606389ea25c65ef40370
silvia <silvia@maykinmedia.nl>
2022-02-08 11:26:03 +0100

    :recycle: DRY generate metadata

--- digid_eherkenning/management/commands/generate_digid_metadata.py
+++ digid_eherkenning/management/commands/generate_digid_metadata.py
@@ -5,85 +5,7 @@ from django.utils import timezone
 from onelogin.saml2.settings import OneLogin_Saml2_Settings
 
 
-def generate_metadata_digid(options):
-
-    setting_dict = {
-        "strict": True,
-        "security": {
-            "signMetadata": True,
-            "authnRequestsSigned": True,
-            "wantAssertionsEncrypted": options["want_assertions_encrypted"],
-            "wantAssertionsSigned": options["want_assertions_signed"],
-            # None sent for digi-id.
-            "wantAttributeStatement": False,
-            "soapClientKey": options["key_file"],
-            "soapClientCert": options["cert_file"],
-            "soapClientPassphrase": options["key_passphrase"],
-            "signatureAlgorithm": options["signature_algorithm"],
-            "digestAlgorithm": options["digest_algorithm"],
-            # For DigiD, if the Metadata file expires, we sent them an update. So
-            # there is no need for an expiry date.
-            "metadataValidUntil": "",
-            "metadataCacheDuration": "",
-            "requestedAuthnContextComparison": "minimum",
-            "requestedAuthnContext": [
-                "urn:oasis:names:tc:SAML:2.0:ac:classes:MobileTwoFactorContract",
-            ],
-        },
-        # Service Provider Data that we are deploying.
-        "sp": {
-            # Identifier of the SP entity  (must be a URI)
-            "entityId": options["entity_id"],
-            # Specifies info about where and how the <AuthnResponse> message MUST be
-            # returned to the requester, in this case our SP.
-            "assertionConsumerService": {
-                "url": options["base_url"] + reverse("digid:acs"),
-                "binding": "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Artifact",
-            },
-            # If you need to specify requested attributes, set a
-            # attributeConsumingService per service. nameFormat, attributeValue and
-            # friendlyName can be omitted
-            "attributeConsumingService": {
-                "index": options["attribute_consuming_service_index"],
-                "serviceName": options["service_name"],
-                "serviceDescription": options["service_description"],
-                "requestedAttributes": [
-                    {
-                        "name": "bsn",
-                        "required": True,
-                    }
-                ],
-            },
-            "NameIDFormat": "urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified",
-            "x509cert": open(options["cert_file"], "r").read(),
-            "privateKey": open(options["key_file"], "r").read(),
-            "privateKeyPassphrase": options["key_passphrase"],
-        },
-    }
-
-    telephone = options["technical_contact_person_telephone"]
-    email = options["technical_contact_person_email"]
-    if telephone and email:
-        setting_dict["contactPerson"] = {
-            "technical": {"telephoneNumber": telephone, "emailAddress": email}
-        }
-
-    if options["organization_url"] and options["organization_name"]:
-        setting_dict["organization"] = {
-            "nl": {
-                "name": options["organization_name"],
-                "displayname": options["organization_name"],
-                "url": options["organization_url"],
-            }
-        }
-
-    saml2_settings = OneLogin_Saml2_Settings(setting_dict, sp_validation_only=True)
-    return saml2_settings.get_sp_metadata()
-
-
-class Command(BaseCommand):
-    help = "Create the DigiD metadata file"
-
+class SamlMetadataBaseCommand(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument(
             "--want_assertions_encrypted",
@@ -148,13 +70,6 @@ class Command(BaseCommand):
             default=None,
         )
         parser.add_argument(
-            "--attribute_consuming_service_index",
-            type=str,
-            action="store",
-            help="Attribute consuming service index, defaults to 1",
-            default="1",
-        )
-        parser.add_argument(
             "--service_name",
             type=str,
             action="store",
@@ -211,16 +126,8 @@ class Command(BaseCommand):
         )
 
     def check_options(self, options: dict) -> None:
-        required_options = [
-            "key_file",
-            "cert_file",
-            "entity_id",
-            "base_url",
-            "service_name",
-            "service_description",
-        ]
         missing_required_options = []
-        for option in required_options:
+        for option in self.required_options:
             if option not in options or not options[option]:
                 missing_required_options.append(option)
 
@@ -233,7 +140,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         self.check_options(options)
 
-        metadata_content = generate_metadata_digid(options)
+        metadata_content = self.generate_metadata(options)
 
         if options["test"]:
             self.stdout.write(metadata_content.decode("utf-8"))
@@ -242,8 +149,7 @@ class Command(BaseCommand):
         if options["output_file"]:
             filename = options["output_file"]
         else:
-            date_string = timezone.now().date().isoformat()
-            filename = f"digid-metadata-{date_string}.xml"
+            filename = self.get_filename()
 
         metadata_file = open(filename, "xb")
         metadata_file.write(metadata_content)
@@ -252,3 +158,105 @@ class Command(BaseCommand):
         self.stdout.write(
             self.style.SUCCESS("Metadata file successfully generated: %s" % filename)
         )
+
+
+class Command(SamlMetadataBaseCommand):
+    help = "Create the DigiD metadata file"
+
+    required_options = [
+        "key_file",
+        "cert_file",
+        "entity_id",
+        "base_url",
+        "service_name",
+        "service_description",
+    ]
+
+    def add_arguments(self, parser):
+        super().add_arguments(parser)
+
+        parser.add_argument(
+            "--attribute_consuming_service_index",
+            type=str,
+            action="store",
+            help="Attribute consuming service index, defaults to 1",
+            default="1",
+        )
+
+    def get_filename(self):
+        date_string = timezone.now().date().isoformat()
+        return f"digid-metadata-{date_string}.xml"
+
+    def generate_metadata(self, options):
+        setting_dict = {
+            "strict": True,
+            "security": {
+                "signMetadata": True,
+                "authnRequestsSigned": True,
+                "wantAssertionsEncrypted": options["want_assertions_encrypted"],
+                "wantAssertionsSigned": options["want_assertions_signed"],
+                # None sent for digi-id.
+                "wantAttributeStatement": False,
+                "soapClientKey": options["key_file"],
+                "soapClientCert": options["cert_file"],
+                "soapClientPassphrase": options["key_passphrase"],
+                "signatureAlgorithm": options["signature_algorithm"],
+                "digestAlgorithm": options["digest_algorithm"],
+                # For DigiD, if the Metadata file expires, we sent them an update. So
+                # there is no need for an expiry date.
+                "metadataValidUntil": "",
+                "metadataCacheDuration": "",
+                "requestedAuthnContextComparison": "minimum",
+                "requestedAuthnContext": [
+                    "urn:oasis:names:tc:SAML:2.0:ac:classes:MobileTwoFactorContract",
+                ],
+            },
+            # Service Provider Data that we are deploying.
+            "sp": {
+                # Identifier of the SP entity  (must be a URI)
+                "entityId": options["entity_id"],
+                # Specifies info about where and how the <AuthnResponse> message MUST be
+                # returned to the requester, in this case our SP.
+                "assertionConsumerService": {
+                    "url": options["base_url"] + reverse("digid:acs"),
+                    "binding": "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Artifact",
+                },
+                # If you need to specify requested attributes, set a
+                # attributeConsumingService per service. nameFormat, attributeValue and
+                # friendlyName can be omitted
+                "attributeConsumingService": {
+                    "index": options["attribute_consuming_service_index"],
+                    "serviceName": options["service_name"],
+                    "serviceDescription": options["service_description"],
+                    "requestedAttributes": [
+                        {
+                            "name": "bsn",
+                            "required": True,
+                        }
+                    ],
+                },
+                "NameIDFormat": "urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified",
+                "x509cert": open(options["cert_file"], "r").read(),
+                "privateKey": open(options["key_file"], "r").read(),
+                "privateKeyPassphrase": options["key_passphrase"],
+            },
+        }
+
+        telephone = options["technical_contact_person_telephone"]
+        email = options["technical_contact_person_email"]
+        if telephone and email:
+            setting_dict["contactPerson"] = {
+                "technical": {"telephoneNumber": telephone, "emailAddress": email}
+            }
+
+        if options["organization_url"] and options["organization_name"]:
+            setting_dict["organization"] = {
+                "nl": {
+                    "name": options["organization_name"],
+                    "displayname": options["organization_name"],
+                    "url": options["organization_url"],
+                }
+            }
+
+        saml2_settings = OneLogin_Saml2_Settings(setting_dict, sp_validation_only=True)
+        return saml2_settings.get_sp_metadata()
