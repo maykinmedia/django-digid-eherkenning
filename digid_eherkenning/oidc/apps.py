5a761af3b920b8701c5b8bcfd152029282c11f20
Sergei Maertens <sergei@maykinmedia.nl>
2024-05-24 17:56:34 +0200

    :sparkles: Specify default_auto_field in backwards compatible manner

--- digid_eherkenning/oidc/apps.py
+++ digid_eherkenning/oidc/apps.py
@@ -7,3 +7,4 @@ class OIDCAppConfig(AppConfig):
     verbose_name = _("DigiD & eHerkenning via OpenID Connect")
     # can't change this label because of existing migrations in Open Forms/Open Inwoner
     label = "digid_eherkenning_oidc_generics"
+    default_auto_field = "django.db.models.AutoField"
