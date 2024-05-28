187359e3416556c56d61ba7a540b1e11f3228997
Sergei Maertens <sergei@maykinmedia.nl>
2024-05-28 14:23:16 +0200

    :hammer: Auto-skip tests if OIDC is not enabled

--- tests/oidc/test_oidc_integration.py
+++ tests/oidc/test_oidc_integration.py
@@ -1,2 +1,11 @@
+from django.conf import settings
+
+import pytest
+
+pytestmark = [
+    pytest.mark.skipif(not settings.OIDC_ENABLED, reason="OIDC integration disabled"),
+]
+
+
 def test_oidc_enabled(settings):
     assert settings.OIDC_ENABLED is True
