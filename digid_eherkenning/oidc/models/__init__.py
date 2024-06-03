0476eab8f1f67440f499415c0206de570fb0dcb7
Sergei Maertens <sergei@maykinmedia.nl>
2024-06-03 14:26:15 +0200

    :sparkles: Export base config model
    So that downstream projects can import this too for type checks
    and issubclass checks.

--- digid_eherkenning/oidc/models/__init__.py
+++ digid_eherkenning/oidc/models/__init__.py
@@ -1,10 +1,15 @@
-from .base import get_default_scopes_bsn, get_default_scopes_kvk
+from .base import (
+    OpenIDConnectBaseConfig,
+    get_default_scopes_bsn,
+    get_default_scopes_kvk,
+)
 from .digid import DigiDConfig, DigiDMachtigenConfig
 from .eherkenning import EHerkenningBewindvoeringConfig, EHerkenningConfig
 
 __all__ = [
     "get_default_scopes_bsn",
     "get_default_scopes_kvk",
+    "OpenIDConnectBaseConfig",
     "DigiDConfig",
     "DigiDMachtigenConfig",
     "EHerkenningConfig",
