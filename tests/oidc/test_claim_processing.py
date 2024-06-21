c24df7db981084910d9bb9e34e6236ff57a44b8e
Sergei Maertens <sergei@maykinmedia.nl>
2024-06-21 15:12:52 +0200

    :white_check_mark: Add tests for DigiD Machtigen claim processing

--- tests/oidc/test_claim_processing.py
+++ tests/oidc/test_claim_processing.py
@@ -71,3 +71,107 @@ def test_loa_claim_absent_without_default_loa():
     result = process_claims(claims, config)
 
     assert result == {"bsn_claim": "XXXXXXX54"}
+
+
+### DIGID MACHTIGEN
+
+
+@pytest.mark.parametrize(
+    "claims,expected",
+    [
+        # BSN extraction + transform loa values
+        (
+            {
+                "representee": "XXXXXXX54",
+                "authorizee": "XXXXXXX99",
+                "authsp_level": "30",
+                "service_id": "46ddda34-c4db-4a54-997c-351bc9a0aabc",
+                "extra": "irrelevant",
+            },
+            {
+                "representee_bsn_claim": "XXXXXXX54",
+                "authorizee_bsn_claim": "XXXXXXX99",
+                "loa_claim": DigiDAssuranceLevels.high,
+                "mandate_service_id_claim": "46ddda34-c4db-4a54-997c-351bc9a0aabc",
+            },
+        ),
+        # BSN extraction + missing loa claim
+        (
+            {
+                "representee": "XXXXXXX54",
+                "authorizee": "XXXXXXX99",
+                "service_id": "46ddda34-c4db-4a54-997c-351bc9a0aabc",
+            },
+            {
+                "representee_bsn_claim": "XXXXXXX54",
+                "authorizee_bsn_claim": "XXXXXXX99",
+                "loa_claim": DigiDAssuranceLevels.middle,
+                "mandate_service_id_claim": "46ddda34-c4db-4a54-997c-351bc9a0aabc",
+            },
+        ),
+        # BSN extraction + unmapped LOA value
+        (
+            {
+                "representee": "XXXXXXX54",
+                "authorizee": "XXXXXXX99",
+                "authsp_level": "20",
+                "service_id": "46ddda34-c4db-4a54-997c-351bc9a0aabc",
+                "extra": "irrelevant",
+            },
+            {
+                "representee_bsn_claim": "XXXXXXX54",
+                "authorizee_bsn_claim": "XXXXXXX99",
+                "loa_claim": "20",
+                "mandate_service_id_claim": "46ddda34-c4db-4a54-997c-351bc9a0aabc",
+            },
+        ),
+    ],
+)
+def test_digid_machtigen_claim_processing(claims: JSONObject, expected: JSONObject):
+    config = DigiDMachtigenConfig(
+        representee_bsn_claim=["representee"],
+        authorizee_bsn_claim=["authorizee"],
+        mandate_service_id_claim=["service_id"],
+        loa_claim=["authsp_level"],
+        default_loa=DigiDAssuranceLevels.middle,
+        loa_value_mapping=[
+            {"from": "30", "to": DigiDAssuranceLevels.high},
+        ],
+    )
+
+    result = process_claims(claims, config)
+
+    assert result == expected
+
+
+@pytest.mark.parametrize(
+    "claims",
+    (
+        {},
+        {
+            "authorizee": "XXXXXXX99",
+            "authsp_level": "30",
+            "service_id": "46ddda34-c4db-4a54-997c-351bc9a0aabc",
+        },
+        {
+            "representee": "XXXXXXX54",
+            "authsp_level": "30",
+            "service_id": "46ddda34-c4db-4a54-997c-351bc9a0aabc",
+        },
+        {
+            "representee": "XXXXXXX54",
+            "authorizee": "XXXXXXX99",
+            "authsp_level": "30",
+        },
+    ),
+)
+def test_digid_machtigen_raises_on_missing_claims(claims: JSONObject):
+    config = DigiDMachtigenConfig(
+        representee_bsn_claim=["representee"],
+        authorizee_bsn_claim=["authorizee"],
+        mandate_service_id_claim=["service_id"],
+        loa_claim=["authsp_level"],
+    )
+
+    with pytest.raises(ValueError):
+        process_claims(claims, config)
